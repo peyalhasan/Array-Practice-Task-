de801fa31066a009b71008ec99f42a7bea633781
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-07 17:00:43 +0600

    Complete Task 1

---
+++ array.js
@@ -0,0 +1,6 @@
+//Task 1 -> ১️⃣ একটা ৫টা রঙের নামের অ্যারে বানাও এবং এর length প্রিন্ট করো।
+
+const color = ['Red', 'Green', 'Blue', 'Purple', 'Black']
+console.log(color.length)
+
+// Task 2 -> ২️⃣ ["Apple", "Banana", "Mango"] কে এক লাইনে string এ পরিণত করো।
