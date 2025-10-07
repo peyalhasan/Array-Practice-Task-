f9afd83facaec512b5da0456428227073e7f43cb
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-07 17:08:54 +0600

    Complete Task 3,4

--- array.js
+++ array.js
@@ -6,5 +6,17 @@ console.log(colors.length);
 // Task 2 -> ২️⃣ ["Apple", "Banana", "Mango"] কে এক লাইনে string এ পরিণত করো।
 
 const fruits = ['Apple', 'Banana', 'Mango'];
-console.log(fruits.toString())
-console.log(fruits.join(','))
+console.log(fruits.toString()) // toString
+console.log(fruits.join(','))  // join
+
+// Task 3 -> ৩️⃣ ["A", "B", "C", "D"] অ্যারে থেকে ৩য় আইটেমটি প্রিন্ট করো।
+
+const letters = ['A', 'B', 'C', 'D']
+console.log(letters.at(2))  // at(index)
+console.log(letters[2])     // [index]
+
+// Task 4 -> ৪️⃣ ["Learning", "JavaScript", "is", "fun"] কে একসাথে করে একটা বাক্য বানাও।
+
+const sentence = ["Learning", "JavaScript", "is", "fun"]
+console.log(sentence.join(' '))
+
